d356274ddd4f05fe5fa63c4a3f70cea8367d1be1
John Medina <john9713medina@hotmail.com>
2023-07-09 16:29:46 -0700

    adds new Home.js header and paragraph

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Zomb's Coffee Shop</h2>
+      <p>Welcome to our little worm filled corner of the Earth!</p>
+    </div>
+  );
+}
+
+export default Home;
