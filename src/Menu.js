16ee0a2266a52eac604151b5c40382652c79a568
John Medina <john9713medina@hotmail.com>
2023-07-09 16:33:09 -0700

    adds basic coffee menu to populate a workable version for the app

---
+++ src/Menu.js
@@ -0,0 +1,18 @@
+
+
+function Menu () {
+    return (
+        <div>
+            <h2>Coffee Menu</h2>
+            <ul>
+                <li>Espresso</li>
+                <li>Cappuccino</li>
+                <li>Latte</li>
+                <li>Americano</li>
+                <li>Frappuccino</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Menu;
