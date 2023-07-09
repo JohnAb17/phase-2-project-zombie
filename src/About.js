4dae04aa9c435c8db85538b51878eebcb885a5b7
John Medina <john9713medina@hotmail.com>
2023-07-09 16:35:47 -0700

    adds some info to populate our about section

---
+++ src/About.js
@@ -0,0 +1,11 @@
+
+function About() {
+    return (
+        <div>
+            <h2>About the Shop</h2>
+            <p>We are an undead coffee shop with drinks looking for a home!</p>
+        </div>
+    );
+}
+
+export default About;
